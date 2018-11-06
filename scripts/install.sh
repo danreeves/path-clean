00a97ad2df4c909ba8e188674acd230d788d323b
Dan Reeves <hey@danreev.es>
2018-11-06 00:53:58 +0000

    dont error if its already installed

--- scripts/install.sh
+++ scripts/install.sh
@@ -31,7 +31,7 @@ main() {
   esac
 
   # This fetches latest stable release
-  cargo install cross
+  cargo install cross || true
 
   # Install test dependencies
   rustup component add rustfmt-preview
