d462b8db1bad59ff7fa76287933fd06235a505bc
Adam Reichold <adamreichold@users.noreply.github.com>
2023-02-24 03:41:42 +0100

    RFC: Lexicographical path cleaning using the path type of the standard library. (#10)

--- src/lib.rs
+++ src/lib.rs
@@ -18,25 +18,25 @@
 //! ```rust
 //! use std::path::PathBuf;
 //! use path_clean::{clean, PathClean};
-//! assert_eq!(clean("hello/world/.."), "hello");
+//! assert_eq!(clean("hello/world/.."), PathBuf::from("hello"));
 //! assert_eq!(
 //!     PathBuf::from("/test/../path/").clean(),
 //!     PathBuf::from("/path")
 //! );
 //! ```
+#![forbid(unsafe_code)]
 
-use std::path::PathBuf;
+use std::path::{Component, Path, PathBuf};
 
-/// The Clean trait implements a `clean` method. It's recommended you use the provided [`clean`]
-/// function.
-pub trait PathClean<T> {
-    fn clean(&self) -> T;
+/// The Clean trait implements a `clean` method.
+pub trait PathClean {
+    fn clean(&self) -> PathBuf;
 }
 
-/// PathClean implemented for PathBuf
-impl PathClean<PathBuf> for PathBuf {
+/// PathClean implemented for `Path`
+impl PathClean for Path {
     fn clean(&self) -> PathBuf {
-        PathBuf::from(clean(self.to_str().unwrap_or("")))
+        clean(self)
     }
 }
 
@@ -48,84 +48,34 @@ impl PathClean<PathBuf> for PathBuf {
 /// 5. Leave intact `..` elements that begin a non-rooted path.
 ///
 /// If the result of this process is an empty string, return the string `"."`, representing the current directory.
-pub fn clean(path: &str) -> String {
-    let out = clean_internal(path.as_bytes());
-    // The code only matches/modifies ascii tokens and leaves the rest of
-    // the bytes as they are, so if the input string is valid utf8 the result
-    // will also be valid utf8.
-    unsafe { String::from_utf8_unchecked(out) }
-}
+pub fn clean<P>(path: P) -> PathBuf
+where
+    P: AsRef<Path>,
+{
+    let mut out = Vec::new();
 
-fn clean_internal(path: &[u8]) -> Vec<u8> {
-    static DOT: u8 = b'.';
-    static SEP: u8 = b'/';
-
-    if path.is_empty() {
-        return vec![DOT];
-    }
-
-    let rooted = path[0] == SEP;
-    let n = path.len();
-
-    // Invariants:
-    //  - reading from path; r is index of next byte to process.
-    //  - dotdot is index in out where .. must stop, either because it is the
-    //    leading slash or it is a leading ../../.. prefix.
-    //
-    // The go code this function is based on handles already-clean paths without
-    // an allocation, but I haven't done that here because I think it
-    // complicates the return signature too much.
-    let mut out: Vec<u8> = Vec::with_capacity(n);
-    let mut r = 0;
-    let mut dotdot = 0;
-
-    if rooted {
-        out.push(SEP);
-        r = 1;
-        dotdot = 1
-    }
-
-    while r < n {
-        if path[r] == SEP || path[r] == DOT && (r + 1 == n || path[r + 1] == SEP) {
-            // empty path element || . element: skip
-            r += 1;
-        } else if path[r] == DOT && path[r + 1] == DOT && (r + 2 == n || path[r + 2] == SEP) {
-            // .. element: remove to last separator
-            r += 2;
-            if out.len() > dotdot {
-                // can backtrack, truncate to last separator
-                let mut w = out.len() - 1;
-                while w > dotdot && out[w] != SEP {
-                    w -= 1;
+    for comp in path.as_ref().components() {
+        match comp {
+            Component::CurDir => (),
+            Component::ParentDir => match out.last() {
+                Some(Component::RootDir) => (),
+                Some(Component::Normal(_)) => {
+                    out.pop();
                 }
-                out.truncate(w);
-            } else if !rooted {
-                // cannot backtrack, but not rooted, so append .. element
-                if !out.is_empty() {
-                    out.push(SEP);
-                }
-                out.push(DOT);
-                out.push(DOT);
-                dotdot = out.len();
-            }
-        } else {
-            // real path element
-            // add slash if needed
-            if rooted && out.len() != 1 || !rooted && !out.is_empty() {
-                out.push(SEP);
-            }
-            while r < n && path[r] != SEP {
-                out.push(path[r]);
-                r += 1;
-            }
+                None
+                | Some(Component::CurDir)
+                | Some(Component::ParentDir)
+                | Some(Component::Prefix(_)) => out.push(comp),
+            },
+            comp => out.push(comp),
         }
     }
 
-    // Turn empty string into "."
-    if out.is_empty() {
-        out.push(DOT);
+    if !out.is_empty() {
+        out.iter().collect()
+    } else {
+        PathBuf::from(".")
     }
-    out
 }
 
 #[cfg(test)]
@@ -135,7 +85,7 @@ mod tests {
 
     #[test]
     fn test_empty_path_is_current_dir() {
-        assert_eq!(clean(""), ".");
+        assert_eq!(clean(""), PathBuf::from("."));
     }
 
     #[test]
@@ -143,7 +93,7 @@ mod tests {
         let tests = vec![(".", "."), ("..", ".."), ("/", "/")];
 
         for test in tests {
-            assert_eq!(clean(test.0), test.1);
+            assert_eq!(clean(test.0), PathBuf::from(test.1));
         }
     }
 
@@ -164,7 +114,7 @@ mod tests {
         ];
 
         for test in tests {
-            assert_eq!(clean(test.0), test.1);
+            assert_eq!(clean(test.0), PathBuf::from(test.1));
         }
     }
 
@@ -180,7 +130,7 @@ mod tests {
         ];
 
         for test in tests {
-            assert_eq!(clean(test.0), test.1);
+            assert_eq!(clean(test.0), PathBuf::from(test.1));
         }
     }
 
@@ -207,7 +157,7 @@ mod tests {
         ];
 
         for test in tests {
-            assert_eq!(clean(test.0), test.1);
+            assert_eq!(clean(test.0), PathBuf::from(test.1));
         }
     }
 
@@ -217,5 +167,22 @@ mod tests {
             PathBuf::from("/test/../path/").clean(),
             PathBuf::from("/path")
         );
+    }
+
+    #[test]
+    #[cfg(target_os = "windows")]
+    fn test_windows_paths() {
+        let tests = vec![
+            ("\\..", "\\"),
+            ("\\..\\test", "\\test"),
+            ("test\\..", "."),
+            ("test\\path\\..\\..\\..", ".."),
+            ("test\\path/..\\../another\\path", "another\\path"), // Mixed
+            ("/dir\\../otherDir/test.json", "/otherDir/test.json"), // User example
+        ];
+
+        for test in tests {
+            assert_eq!(clean(test.0), PathBuf::from(test.1));
+        }
     }
 }
